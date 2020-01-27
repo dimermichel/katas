fc3ae2f1c9a76916e030e7b60a578e1740c5bef8
Michel Maia <dimermichel@gmail.com>
2020-01-26 23:18:05 -0500

    Add find the lucky number kata solution

---
+++ find-the-lucky-number.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/580435ab150cca22650001fb/javascript
+
+// Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+
+// For example,
+
+// ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+// [7,70,17]
+// Don't worry about bad input, you will always receive a finite list of integers.
+
+let filterLucky = x => {
+    let arr = x.slice().toString().split(',')
+    let result = arr.filter(el => el.includes('7')).map(el => Number(el))
+    return result;
+  }
+
+let x = [1,2,3,4,5,6,7,68,69,70,15,17]
+
+console.log(filterLucky(x));
